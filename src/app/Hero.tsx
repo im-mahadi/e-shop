b2ce5a56b8f430650d3d5fdd82cb3a8523355609
im-mahadi <me.mahadi@outlook.com>
2023-02-19 01:47:59 +0600

    add hero section

---
+++ src/app/Hero.tsx
@@ -0,0 +1,29 @@
+'use client';
+
+import { Button } from 'flowbite-react';
+import Image from 'next/image';
+
+export default function Hero() {
+  return (
+    <div className='mt-6 flex max-h-72 justify-around rounded-lg bg-rose-100'>
+      <div className='flex flex-col items-start justify-center gap-5'>
+        <p className='text-3xl font-bold text-green-800'>
+          Grap Upto 50% off on <br />
+          the Selected Headphone
+        </p>
+        <Button color='success' pill={true}>
+          Buy Now
+        </Button>
+      </div>
+      <div>
+        <Image
+          src='/hero.png'
+          className='relative bottom-12'
+          alt='hero'
+          width={600}
+          height={500}
+        />
+      </div>
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
+import Hero from './Hero';
+
 export default function Home() {
-  return <></>;
+  return (
+    <div className='mx-auto w-11/12'>
+      <Hero />
+    </div>
+  );
 }
