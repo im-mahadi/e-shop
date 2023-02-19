27b36f44304c2da19c95839cc323a27495e37213
im-mahadi <me.mahadi@outlook.com>
2023-02-19 19:10:38 +0600

    refactor: hero section

--- src/app/Hero.tsx
+++ src/app/Hero.tsx
@@ -5,7 +5,7 @@ import Image from 'next/image';
 
 export default function Hero() {
   return (
-    <div className='mt-6 flex max-h-72 justify-around rounded-lg bg-rose-100'>
+    <div className='mt-6 flex max-h-72 justify-evenly rounded-lg bg-rose-100'>
       <div className='flex flex-col items-start justify-center gap-5'>
         <p className='text-3xl font-bold text-green-800'>
           Grap Upto 50% off on <br />
